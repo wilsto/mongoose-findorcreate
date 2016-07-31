2eb72a28ccd1d74fdceae855d6445732c9cee4ed
wilsto <willy.stophe.pro@gmail.com>
2016-07-31 11:25:13 +0200

    update save function

--- index.js
+++ index.js
@@ -5,42 +5,38 @@
  */
 
 function findOrCreatePlugin(schema, options) {
-  schema.statics.findOrCreate = function findOrCreate(conditions, doc, options, callback) {
-    if (arguments.length < 4) {
-      if (typeof options === 'function') {
-        // Scenario: findOrCreate(conditions, doc, callback)
-        callback = options;
-        options = {};
-      } else if (typeof doc === 'function') {
-        // Scenario: findOrCreate(conditions, callback);
-        callback = doc;
-        doc = {};
-        options = {};
-      }
-    }
-    var self = this;
-    this.findOne(conditions, function(err, result) {
-      if(err || result) {
-        if(options && options.upsert && !err) {
-          self.update(conditions, doc, function(err, count){
-            self.findOne(conditions, function(err, result) {
-              callback(err, result, false);
-            });
-          })
-        } else {
-          callback(err, result, false)
+    schema.statics.findOrCreate = function findOrCreate(conditions, doc, options, callback) {
+        if (arguments.length < 4) {
+            if (typeof options === 'function') {
+                // Scenario: findOrCreate(conditions, doc, callback)
+                callback = options;
+                options = {};
+            } else if (typeof doc === 'function') {
+                // Scenario: findOrCreate(conditions, callback);
+                callback = doc;
+                doc = {};
+                options = {};
+            }
         }
-      } else {
-        for (var key in doc) {
-         conditions[key] = doc[key]; 
-        }
-        var obj = new self(conditions)
-        obj.save(function(err) {
-          callback(err, obj, true);
+        var self = this;
+        this.findOne(conditions, function(err, result) {
+            if (err || result) {
+                if (options && options.upsert && !err) {
+                    self.update(conditions, doc, function(err, count) {
+                        self.findOne(conditions, function(err, result) {
+                            callback(err, result, false);
+                        });
+                    });
+                } else {
+                    callback(err, result, false);
+                }
+            } else {
+                doc.save(function(err) {
+                    callback(err, doc, true);
+                });
+            }
         });
-      }
-    })
-  }
+    };
 }
 
 /**
